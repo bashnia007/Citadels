a604003d52efbff36a6a8c847c6be9e00aef7e5c
Aleksandr Kavokin <alexanderkavokin@gmail.com>
2017-05-05 13:48:51 +0300

    Добавил обработчики на кнопки лобби

--- CitadelsApp/CitadelsApp/LobbyViewModel.cs
+++ CitadelsApp/CitadelsApp/LobbyViewModel.cs
@@ -13,26 +13,72 @@ namespace CitadelsApp
 {
     public class LobbyViewModel : PropertyChangedBase
     {
+        #region Properties
+        public ObservableCollection<Game> Games { get; set; }
+        #endregion
+
+        #region Constructors
         public LobbyViewModel()
         {
         }
-        
+        #endregion
 
-        public ObservableCollection<Game> Games { get; set; }
+        #region Commands
 
-        private ICommand _refreshCommand;
+        #region RefreshCommand
+        private RelayCommand _refreshCommand;
 
-        public async void GetGames()
+        public ICommand RefreshCommand => _refreshCommand ??
+                                          (_refreshCommand = new RelayCommand(ExecuteRefreshCommand, CanExecuteRefreshCommand));
+
+        public async void ExecuteRefreshCommand(object param)
         {
             var data = await ServiceProxy.GetAvaivableGames();
             Games = new ObservableCollection<Game>(data);
         }
 
-        public ICommand RefreshCommand { get; set; }
+        private bool CanExecuteRefreshCommand(object param)
+        {
+            return true;
+        }
+        #endregion
 
-        public async void CreateGame()
+        #region ConnectCommand
+
+        private RelayCommand _connectCommand;
+        public ICommand ConnectCommand => _connectCommand ?? 
+                                          (_connectCommand = new RelayCommand(ExecuteConnectCommand, CanExecuteConnectCommand));
+
+        private async void ExecuteConnectCommand(object param)
         {
             
         }
+
+        private bool CanExecuteConnectCommand(object param)
+        {
+            return true;
+        }
+
+        #endregion
+
+        #region CreateGameCommand
+
+        private RelayCommand _createGameCommand;
+        public ICommand CreateGameCommand => _createGameCommand ?? 
+                                             (_createGameCommand = new RelayCommand(ExecuteCreateGameCommand, CanExecuteCreateGameCommand));
+
+        private void ExecuteCreateGameCommand(object param)
+        {
+            
+        }
+
+        private bool CanExecuteCreateGameCommand(object param)
+        {
+            return true;
+        }
+
+        #endregion
+
+        #endregion
     }
 }
